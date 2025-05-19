0f99ca61c83046741de69f9cb39d3a13fe866838
md-zeon <zeonrahaman5870@gmail.com>
2025-05-20 01:03:09 +0600

    added Navbar with links

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div>
+			<Navbar />
+			<div className='max-w-7xl mx-auto'>
+				<Outlet />
+			</div>
+		</div>
+	);
 };
 
 export default MainLayout;
