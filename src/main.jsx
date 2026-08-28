92010de5ccd74a023a485f795c1175bc08ffdc61
md-zeon <zeon.cse@gmail.com>
2026-08-28 12:41:07 +0600

    feat: revamp theme with warm neutrals and Plus Jakarta Sans

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/routes.jsx";
-import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./context/AuthProvider.jsx";
 import { ToastContainer } from "react-toastify";
-import AOS from "aos";
-AOS.init({ duration: 1000 });
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
-	<>
+	<StrictMode>
 		<AuthProvider>
-			<HelmetProvider>
-				<RouterProvider router={router} />
-			</HelmetProvider>
+			<RouterProvider router={router} />
 		</AuthProvider>
-		<ToastContainer />
-	</>,
+		<ToastContainer
+			position='top-center'
+			autoClose={3000}
+			hideProgressBar
+			theme='colored'
+			limit={3}
+		/>
+	</StrictMode>,
 );
